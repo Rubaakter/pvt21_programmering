5afbc1d580fba03d10485ad0e06b08d9d97842e9
bjorn <bjorn@bjornkjellgren.se>
2021-09-16 15:03:01 +0200

    Additional tasks for week 1

---
+++ vecka1/uppgift10.py
@@ -0,0 +1,6 @@
+# Skriv flera program t.ex. ug10_1.py, ug10_2.py som skriver ut:
+# 1) Heltalen från och med 1 till och med 10 i ökande ordning
+# 2) Udda heltal från och med 1 till och med 49 i ökande ordning
+# 3) Heltal från och med 10 till och med 1 i minskande ordning
+# 4) Summan av talen 7 till och med 1000
+# 5) produkten av talen 1 till och med 1000
