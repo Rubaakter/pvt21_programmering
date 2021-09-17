e0dce32db0d218e7d50967d1d7f8cdf82a8114e5
bjorn <bjorn@bjornkjellgren.se>
2021-09-17 10:03:11 +0200

    Lade till möjlighet att göra multiplikation

--- vecka1/uppgift6.py
+++ vecka1/uppgift6.py
@@ -11,7 +11,7 @@ def subtract(a, b):
 
 
 def multiply(a, b):
-    result = a + b
+    result = a * b
     return result
 
 
@@ -23,14 +23,16 @@ def run():
     print("This program will help with simple calculations. What do you want to do?")
     print("1 - add numbers")
     print("2 - subtract numbers")
+    print("3 - multiply numbers")
     answer = input(">>")
     a = int(input("A="))
     b = int(input("B="))
-
     if answer == "1":
         result = add(a, b)
     elif answer == "2":
         result = subtract(a, b)
+    elif answer == "3":
+        result = multiply(a, b)
     else:
         result = ""
 
