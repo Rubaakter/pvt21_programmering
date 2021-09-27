abd891ca2089f7a0481f3ef27e2226e09f8537b1
bjorn <bjorn@bjornkjellgren.se>
2021-09-27 08:18:32 +0200

    lade till repetition under vecka 3

---
+++ Vecka 3/repetition.py
@@ -0,0 +1,40 @@
+
+min_var = "Detta är en textsträng"
+
+# Vad kommer följande kod göra
+print(min_var)
+
+var1 = 10
+var2 = 5
+
+print(var1 + var2)
+print(var1 * var2)
+print(var1 / var2)
+
+var3 = var1 - var2
+print(var3)
+##########################################################
+lista = [1, 2, 3, 4, 5]
+# Hur vet jag hur lång listan är?
+# Hur kan jag lägga till ett element i listan?
+# Hur kan jag ta bort ett element i listan?
+##########################################################
+dictionary = {"nyckel": "Värde"}
+
+# Vad skriver följande kod ut?
+print(dictionary["nyckel"])
+
+# Hur kan jag ändra ett existerande värde?
+# Hur kan jag lägga till ett nytt nycke-värde par?
+# Hur kan jag ta bort ett värde ur dictionaryn
+
+
+# Vad gör följande kod?
+def add(a, b):
+    return a+b
+
+
+
+# Vilken nytta gör den här koden
+if __name__ == '__main__':
+    print(add(5,1))
